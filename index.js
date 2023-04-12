28758fccacce5f019d4253ef67dc232aec7f2bd4
binisha <binishabajracharya708@gmail.com>
2023-04-12 16:51:46 +0545

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+const submit=document.querySelector('.btn primary');
+const first_name=document.querySelector('#firstName');
+const last_name=document.querySelector('#lastName');
+const email=document.querySelector('#email');
+const password=document.querySelector('#password');
+const  form_group=document.querySelector('.form_group');
+
