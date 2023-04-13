d2aa83483eef813aec6cb46a8fbe3cbd86517084
binisha <binishabajracharya708@gmail.com>
2023-04-13 12:13:50 +0545

    final

--- index.js
+++ index.js
@@ -5,21 +5,24 @@ let email = document.querySelector('#email');
 let password = document.querySelector('#password');
 const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-let validation=()=> {
+let validation=(event)=> {
+  event.preventDefault();
   if (email.value == '') {
     email.setAttribute('style', 'color:hsl(0, 100%, 74%)');
-    email.innerHTML = 'Email cannot be empty';
+    document.querySelector(".email_error").innerHTML="Filed cannot be empty";
     email.setAttribute(
       'style',
       'border-color:hsl(0, 100%, 74%);margin-bottom:3rem'
     );
   } else {
     if (email.value.match(pattern)) {
+      event.preventDefault();
       email.innerHTML = '';
       email.setAttribute('style', 'border-color:hsl(154, 68%, 32%)');
     } else {
       email.setAttribute('style', 'color:hsl(0, 100%, 74%)');
-      email.innerHTML = 'invalid email';
+      ddocument.querySelector(".email_error").innerHTML="Inavlid email pattern";
+      
       email.setAttribute(
         'style',
         'border-color:hsl(0, 100%, 74%);margin-bottom:3rem;font-style:italic'
@@ -28,24 +31,24 @@ let validation=()=> {
   }
 
   if (firstName.value.length == '') {
+    event.preventDefault();
     firstName.setAttribute('style', 'color:hsl(0, 100%, 74%)');
-    firstName.innerHTML = 'First Name cannot be empty';
+document.querySelector(".firstName_error").innerHTML="field shouldn't be empty"
     firstName.setAttribute(
       'style',
-      'border-color:hsl(0, 100%, 74%);margin-bottom:3rem;font-style:italic'
-
-    );
+      'border-color:hsl(0, 100%, 74%)');
   } else {
     firstName.innerHTML = '';
     firstName.setAttribute('style', 'border-color:hsl(154, 68%, 32%)');
   }
 
   if (lastName.value.length == '') {
+    event.preventDefault();
     lastName.setAttribute('style', 'color:hsl(0, 100%, 74%)');
-    lastName.innerHTML = 'Last Name cannot be empty';
+    document.querySelector(".lastName_error").innerHTML="field shouldn't be empty"
     lastName.setAttribute(
       'style',
-      'border-color:hsl(0, 100%, 74%);margin-bottom:3rem;font-style:italic'
+      'border-color:hsl(0, 100%, 74%)'
     );
   } else {
     lastName.innerHTML = '';
@@ -53,14 +56,16 @@ let validation=()=> {
   }
 
   if (password.value.length == '') {
+    event.preventDefault();
     password.setAttribute('style', 'color:hsl(0, 100%, 74%)');
-    password.innerHTML = 'Password cannot be empty';
+    document.querySelector(".password_error").innerHTML="field shouldn't be empty"
     password.setAttribute(
       'style',
-      'border-color:hsl(0, 100%, 74%);margin-bottom:3rem;font-style:italic'
+      'border-color:hsl(0, 100%, 74%)'
     );
   } else {
     password.innerHTML = '';
-    password.setAttribute('style', 'border-color:hsl(154, 68%, 32%)');
+    password.setAttribute('style', 'border-color:hsl(0, 100%, 74%)'
+    );
   }
 }
